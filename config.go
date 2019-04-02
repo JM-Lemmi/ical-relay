7c565d08e68b154d196e57cea2510ea9fd33083a
Thor77 <thor77@thor77.org>
2019-04-02 12:47:59 +0200

    Initial commit

---
+++ config.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"regexp"
+
+	"github.com/BurntSushi/toml"
+)
+
+type regex struct {
+	regexp.Regexp
+}
+
+type profile struct {
+	RegEx  []string
+	Public bool
+}
+
+// Config represents configuration for the application
+type Config struct {
+	URL      string
+	Profiles map[string]profile
+}
+
+func (r regex) UnmarshalText(text []byte) error {
+	tmpRe, err := regexp.Compile("(?i)" + string(text))
+	r.Regexp = *tmpRe
+	return err
+}
+
+// ParseConfig reads config from path and returns a Config struct
+func ParseConfig(path string) (Config, error) {
+	var tmpConfig Config
+
+	if _, err := toml.DecodeFile(path, &tmpConfig); err != nil {
+		return tmpConfig, err
+	}
+
+	return tmpConfig, nil
+}
---
+++ handlers.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+
+	"github.com/arran4/golang-ical"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	requestLogger := log.WithFields(log.Fields{"request": uuid.New().String()})
+	// load profile
+	vars := mux.Vars(r)
+	profileName := vars["profile"]
+	profile, ok := conf.Profiles[profileName]
+	if !ok {
+		errorMsg := fmt.Sprintf("Profile '%s' doesn't exist", profileName)
+		requestLogger.Infoln(errorMsg)
+		http.Error(w, errorMsg, 404)
+		return
+	}
+	// request original ical
+	response, err := http.Get(conf.URL)
+	if err != nil {
+		// throw error
+		requestLogger.Errorln(err)
+	}
+	// parse original calendar
+	calendar, err := ics.ParseCalendar(response.Body)
+	if err != nil {
+		requestLogger.Errorln(err)
+	}
+	// create new calendar, excluding based on pattern
+	newCalendar := ics.NewCalendar()
+	newCalendar.CalendarProperties = calendar.CalendarProperties
+	excludedEvents := 0
+	for _, event := range calendar.Events() {
+		// extract summary from original event
+		summary := event.GetProperty(ics.ComponentPropertySummary).Value
+		// check if one of the profiles regex's matches summary
+		exclude := false
+		for _, excludeRe := range profile.RegEx {
+
+			if m, _ := regexp.MatchString("(?i)"+excludeRe, summary); m {
+				exclude = true
+				break
+			}
+		}
+		if !exclude {
+			// add event to new calendar
+			newEvent := newCalendar.AddEvent(event.Id())
+			// copy properties from original event
+			newEvent.Properties = event.Properties
+		} else {
+			excludedEvents++
+			requestLogger.Printf("Excluding event with summary '%s'\n", summary)
+		}
+	}
+	// make sure new calendar has all events but excluded
+	eventCountDiff := len(newCalendar.Events()) + excludedEvents - len(calendar.Events())
+	if eventCountDiff == 0 {
+		requestLogger.Printf("Output validation successfull; event counts match")
+	} else {
+		requestLogger.Warnf("This shouldn't happen, event count diff: %d", eventCountDiff)
+	}
+	requestLogger.Printf("Excluded %d events", excludedEvents)
+	// return new calendar
+	w.Header().Set("Content-Type", "text/calendar; charset=utf-8")
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s.ics", profileName))
+	fmt.Fprint(w, newCalendar.Serialize())
+}
