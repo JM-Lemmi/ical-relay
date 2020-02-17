b6861cf20b1c424a5ae1ab50cc5cc0fd85eacc54
Thor77 <thor77@thor77.org>
2020-02-17 11:39:50 +0100

    Add service and postinstall file

---
+++ misc/postinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if ! getent group ical-relay > /dev/null 2>&1 ; then
+    addgroup --system ical-relay --quiet
+fi
+if ! id ical-relay > /dev/null 2>&1 ; then
+    adduser --system --no-create-home \
+        --ingroup ical-relay --disabled-password --shell /bin/false \
+        ical-relay
+fi
