71eee94ed8f4dffa2958d5d042595035b7a1462e
Julian Lemmerich <s212689@student.dhbw-mannheim.de>
2022-01-31 15:51:26 +0100

    Bugfix: Use AddVEvent Fix #8 and #9

--- handlers.go
+++ handlers.go
@@ -98,16 +98,9 @@ func profileHandler(w http.ResponseWriter, r *http.Request) {
 				h.Write([]byte(event.Id()))
 				h.Write([]byte(profile.URL))
 				id = fmt.Sprintf("%x@%s", h.Sum(nil), "ical-relay")
+				event.SetProperty(ics.ComponentPropertyUniqueId, id)
 			}
-			newEvent := newCalendar.AddEvent(id)
-			// exclude organizer, uuid, attendee property due to broken escaping
-			for _, property := range event.Properties {
-				if (property.IANAToken != string(ics.ComponentPropertyOrganizer)) && (property.IANAToken != string(ics.ComponentPropertyUniqueId) && (property.IANAToken != string(ics.ComponentPropertyAttendee))) {
-					newEvent.Properties = append(newEvent.Properties, property)
-				}
-			}
-			sequenceProperty := ics.IANAProperty{BaseProperty: ics.BaseProperty{IANAToken: "SEQUENCE", Value: "0"}}
-			newEvent.Properties = append(newEvent.Properties, sequenceProperty)
+			newCalendar.AddVEvent(event)
 		} else {
 			excludedEvents++
 			requestLogger.Debugf("Excluding event '%s' with id %s\n", summary, id)
