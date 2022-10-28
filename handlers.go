5edd8bb84f0cc12e8909a032f9c79894786b5251
Frederik Reiter <kontakt@frederikreiter.de>
2022-10-28 22:57:56 +0200

    Add javascript to cycle through months

--- handlers.go
+++ handlers.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 
+	ics "github.com/arran4/golang-ical"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 )
 
 var htmlTemplates = template.Must(template.ParseGlob("templates/*.html"))
+
+type eventData map[string]interface{}
+type calendarDataByDay map[time.Time][]eventData
 
 func tryRenderErrorOrFallback(w http.ResponseWriter, r *http.Request, statusCode int, err error, fallback string) {
 	requestLogger := log.WithFields(log.Fields{"client": GetIP(r)})
@@ -54,11 +59,51 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 		tryRenderErrorOrFallback(w, r, http.StatusInternalServerError, err, "Internal Server Error")
 		return
 	}
+	allEvents := getEventsByDay(calendar)
+	filteredEvents := make(calendarDataByDay)
+
+	month_start := time.Date(time.Now().Year(), time.Now().Month(), 1, 0, 0, 0, 0, time.UTC)
+	calendar_start := month_start.AddDate(0, 0, -int(month_start.Weekday())+1)
+	month_end := month_start.AddDate(0, 1, 0)
+	calendar_end := month_end.AddDate(0, 0, 7-int(month_end.Weekday()))
+
+	for day := calendar_start; day.Before(calendar_end); day = day.AddDate(0, 0, 1) {
+		if day.Weekday() != time.Sunday {
+			filteredEvents[day] = allEvents[day]
+		}
+	}
+
 	htmlTemplates.ExecuteTemplate(w, "view.html", map[string]interface{}{
-		"ProfileName": profileName,
-		"Calendar":    calendar.Events(),
-		"Profiles":    getProfilesMetadata(),
+		"ProfileName":     profileName,
+		"CurrentMonth":    month_start,
+		"EventsThisMonth": filteredEvents,
+		"AllEvents":       allEvents,
+		"Profiles":        getProfilesMetadata(),
 	})
+}
+
+func getEventsByDay(calendar *ics.Calendar) calendarDataByDay {
+	calendarDataByDay := make(calendarDataByDay)
+	for _, event := range calendar.Events() {
+		startTime, err := event.GetStartAt()
+		if err != nil {
+			log.Errorln(err)
+			continue
+		}
+		endTime, err := event.GetEndAt()
+		if err != nil {
+			log.Errorln(err)
+			continue
+		}
+		day := time.Date(startTime.Year(), startTime.Month(), startTime.Day(), 0, 0, 0, 0, time.UTC)
+		calendarDataByDay[day] = append(calendarDataByDay[day], eventData{
+			"title":    event.GetProperty("SUMMARY").Value,
+			"location": event.GetProperty("LOCATION").Value,
+			"start":    startTime,
+			"end":      endTime,
+		})
+	}
+	return calendarDataByDay
 }
 
 func profileHandler(w http.ResponseWriter, r *http.Request) {
