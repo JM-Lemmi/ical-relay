4d18f27caa97ac9d46c7014baaa1578173170472
Julian Lemmerich <jle@cirosec.de>
2022-12-15 12:04:26 +0100

    add better debug messages to notifiers

--- notifier.go
+++ notifier.go
@@ -33,13 +33,13 @@ func notifyChanges(id string, n *notifier) error {
 	file1, _ := os.Open(notifystore + id + ".ics")
 	calendar1, err := ics.ParseCalendar(file1)
 	if err != nil {
-		requestLogger.Errorln(err)
+		requestLogger.Errorln("error parsing calendar1 file: " + err.Error())
 		return err
 	}
 
 	calendar2, err := readCalURL(n.Source)
 	if err != nil {
-		requestLogger.Errorln(err)
+		requestLogger.Errorln("error parsing calendar2 file: " + err.Error())
 		return err
 	}
 
@@ -88,7 +88,7 @@ func notifyChanges(id string, n *notifier) error {
 
 			err := d.DialAndSend(m)
 			if err != nil {
-				requestLogger.Errorln(err)
+				requestLogger.Errorln("error sending mail: " + err.Error())
 				return err
 			}
 		}
