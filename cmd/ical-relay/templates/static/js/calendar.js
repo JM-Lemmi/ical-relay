5b8e0bc5d5b5006cc867a0941e947c48804ed6c1
Nils Fast <mail@nilsfast.com>
2024-02-09 15:48:32 +0100

    fix: enable editing for events that havent started yet

--- cmd/ical-relay/templates/static/js/calendar.js
+++ cmd/ical-relay/templates/static/js/calendar.js
@@ -75,8 +75,9 @@ function getEventCard(event, show_edit = false, edit_enabled = true) {
     return event_card;
 }
 
-function getDayVStack(date, events, show_edit = false, edit_enabled = true) {
+function getDayVStack(date, events, show_edit = false, immutable_past = true) {
     // VStack = Column of events for a day
+    // stash || date >= dayjs().startOf("day")
     let day_vstack = document.createElement("div");
     document.createElement("div");
     day_vstack.classList.add("vstack", "col-md-4", "col-xl-2", "pt-2", "day-column", "mb-3");
@@ -102,6 +103,7 @@ function getDayVStack(date, events, show_edit = false, edit_enabled = true) {
             return dayjs(a.start).diff(dayjs(b.start));
         });
         for (let event of day_events) {
+            let edit_enabled = !immutable_past || dayjs(event.start).isAfter(dayjs());
             let card = getEventCard(event, show_edit, edit_enabled);
             if (currentType === "month" && date.format("MM") != currentMonth) {
                 card.style.backgroundColor = "#e1e6ea";
