954a7f6a6e2e47e1822b4691f34dc34ac72bdad8
Tim (robske_110) <robske110@users.noreply.github.com>
2024-02-13 20:57:38 +0100

    build: allow generate-version.sh to be run from anywehre

--- .github/scripts/generate-version.sh
+++ .github/scripts/generate-version.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-# also embedded in Dockerfile!
+DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+cd "$DIR" || exit 1
 
 if [[ $(git tag -l --contains HEAD) ]]; then
-    echo -n $(git tag -l --contains HEAD) > ./cmd/ical-relay/VERSION
+    echo -n $(git tag -l --contains HEAD) > ../.././cmd/ical-relay/VERSION
 else
-    echo -n $(git rev-parse --short HEAD) > ./cmd/ical-relay/VERSION
+    echo -n $(git rev-parse --short HEAD) > ../.././cmd/ical-relay/VERSION
 fi
