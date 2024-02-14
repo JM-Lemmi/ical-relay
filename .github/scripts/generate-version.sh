efd150754fc8c9367e8995fd21ec4e90aeeb137c
Julian Lemmerich <jle@cirosec.de>
2024-02-14 12:30:11 +0100

    ci: run go generate in ci and dockerfile

--- .github/scripts/generate-version.sh
+++ .github/scripts/generate-version.sh
@@ -3,10 +3,10 @@ DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$DIR" || exit 1
 
 if [[ $(git tag -l --contains HEAD) ]]; then
-    echo -n $(git tag -l --contains HEAD) > ../.././cmd/ical-relay/VERSION
+    echo -n $(git tag -l --contains HEAD) > ../../cmd/ical-relay/VERSION
 else
-    echo -n $(git rev-parse --short HEAD) > ../.././cmd/ical-relay/VERSION
+    echo -n $(git rev-parse --short HEAD) > ../../cmd/ical-relay/VERSION
 fi
 if ! git diff --quiet; then
-    echo -n "-dirty" >> ../.././cmd/ical-relay/VERSION
+    echo -n "-dirty" >> ../../cmd/ical-relay/VERSION
 fi
