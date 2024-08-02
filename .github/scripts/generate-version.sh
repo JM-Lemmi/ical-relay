b59fc3beba51461dbcb98b60e947b65566881802
Julian Lemmerich <jle@cirosec.de>
2024-08-02 21:03:03 +0200

    ci(notifier): add notifier path in generate script

--- .github/scripts/generate-version.sh
+++ .github/scripts/generate-version.sh
@@ -4,9 +4,12 @@ cd "$DIR" || exit 1
 
 if [[ $(git tag -l --contains HEAD) ]]; then
     echo -n $(git tag -l --contains HEAD) > ../../cmd/ical-relay/VERSION
+    echo -n $(git tag -l --contains HEAD) > ../../cmd/ical-notifier/VERSION
 else
     echo -n $(git rev-parse --short HEAD) > ../../cmd/ical-relay/VERSION
+    echo -n $(git rev-parse --short HEAD) > ../../cmd/ical-notifier/VERSION
 fi
 if ! git diff --quiet; then
     echo -n "-dirty" >> ../../cmd/ical-relay/VERSION
+    echo -n "-dirty" >> ../../cmd/ical-notifier/VERSION
 fi
