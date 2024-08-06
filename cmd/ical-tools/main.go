a7644d490abdce057687893cf13740f6a309f65f
Julian Lemmerich <jle@cirosec.de>
2024-08-06 13:44:41 +0200

    move subcommand to its own file

--- cmd/ical-tools/main.go
+++ cmd/ical-tools/main.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/alexflint/go-arg"
 	ics "github.com/arran4/golang-ical"
-	"github.com/jm-lemmi/ical-relay/compare"
 	"github.com/jm-lemmi/ical-relay/helpers"
 	log "github.com/sirupsen/logrus"
 )
@@ -20,13 +19,9 @@ func main() {
 	log.Info("Welcome to ical-tools, version " + version)
 
 	// CLI Flags
-	// the type xxCmd is used to define subcommands
-	type CompareCmd struct {
-		Base     string `arg:"positional" help:"Base calendar to compare against (include protocol, e.g. file:// or http://)"`
-		Incoming string `arg:"positional" help:"Incoming calendar to compare against (include protocol, e.g. file:// or http://)"`
-	}
+	// subcommands are defined in the respective files
 	var args struct {
-		Compare      *CompareCmd `arg:"subcommand:compare" help:"Compare two ical files"`
+		Compare      *compareCmd `arg:"subcommand:compare" help:"Compare two ical files"`
 		Verbose      bool        `arg:"-v,--verbose" help:"verbosity level Debug"`
 		Superverbose bool        `arg:"--superverbose" help:"verbosity level Trace"`
 	}
@@ -43,44 +38,7 @@ func main() {
 	// starting subcommands
 	switch {
 	case args.Compare != nil:
-		log.Debug("Comparing ical files %s and %s", args.Compare.Base, args.Compare.Incoming)
-
-		base, err := getSource(args.Compare.Base)
-		if err != nil {
-			log.Fatal(err)
-		}
-		incoming, err := getSource(args.Compare.Incoming)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		added, deleted, changed := compare.Compare(base, incoming)
-
-		if len(added)+len(deleted)+len(changed) == 0 {
-			log.Info("No changes detected.")
-		} else {
-			log.Debug("Changes detected: " + fmt.Sprint(len(added)) + " added, " + fmt.Sprint(len(deleted)) + " deleted, " + fmt.Sprint(len(changed)) + " changed")
-
-			var body string
-
-			if len(added) > 0 {
-				for _, event := range added {
-					body += "Added:\n\n" + helpers.PrettyPrint(event) + "\n\n"
-				}
-			}
-			if len(deleted) > 0 {
-				for _, event := range deleted {
-					body += "Deleted:\n\n" + helpers.PrettyPrint(event) + "\n\n"
-				}
-			}
-			if len(changed) > 0 {
-				for _, event := range changed {
-					body += "Changed (displaying new version):\n\n" + helpers.PrettyPrint(event) + "\n\n"
-				}
-			}
-
-			fmt.Println(body)
-		}
+		cmdCompare(*args.Compare)
 	}
 }
 
