50ae5549f29028a5622445a767c6b6ec94d4b008
Julian Lemmerich <jle@cirosec.de>
2024-08-06 14:11:31 +0200

    ci(tools): add version generator and flag

--- cmd/ical-tools/main.go
+++ cmd/ical-tools/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	_ "embed"
+
 	"bytes"
 	"encoding/base64"
 	"fmt"
@@ -13,18 +15,27 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var version = "2.0.0-beta.6.3"
+// CLI Flags
+// subcommands are defined in the respective files
+type args struct {
+	Compare      *compareCmd   `arg:"subcommand:compare" help:"Compare two ical files"`
+	EventInfo    *eventinfoCmd `arg:"subcommand:eventinfo" help:"Get info for a specific event in a calendar"`
+	Verbose      bool          `arg:"-v,--verbose" help:"verbosity level Debug"`
+	Superverbose bool          `arg:"--superverbose" help:"verbosity level Trace"`
+}
+
+//go:generate ../../.github/scripts/generate-version.sh
+//go:embed VERSION
+var version string // If you are here due to a compile error, run go generate
+
+func (args) Version() string {
+	return "ical-tools " + version
+}
 
 func main() {
-	log.Info("Welcome to ical-tools, version " + version)
+	log.Debug("Welcome to ical-tools, version " + version)
 
-	// CLI Flags
-	// subcommands are defined in the respective files
-	var args struct {
-		Compare      *compareCmd `arg:"subcommand:compare" help:"Compare two ical files"`
-		Verbose      bool        `arg:"-v,--verbose" help:"verbosity level Debug"`
-		Superverbose bool        `arg:"--superverbose" help:"verbosity level Trace"`
-	}
+	var args args
 	arg.MustParse(&args)
 
 	// set log level
