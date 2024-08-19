87608abf0841b48d8594f845acc9588b55739db7
Julian Lemmerich <jle@cirosec.de>
2024-08-19 16:16:26 +0200

    ci(test): remove redundant docker build

--- .github/sys-tests/testcase-1/test.sh
+++ .github/sys-tests/testcase-1/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker compose up --build -d --wait
+docker compose up -d --wait
 
 curl -X POST http://localhost:8080/api/profiles/test -H "Authorization: supersecret" --data "{\"sources\":[\"base64://$(base64 -w0 ../../testdata/basic.ics)\"],\"public\": false,\"immutable-past\": true}"
 
