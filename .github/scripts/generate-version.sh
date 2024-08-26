194974481b3eca9c083b6c57ecbbf6bd64e90b8e
Julian Lemmerich <jle@cirosec.de>
2024-08-26 14:51:28 +0200

    ci(pkg): always include latest tag in version

--- .github/scripts/generate-version.sh
+++ .github/scripts/generate-version.sh
@@ -6,8 +6,12 @@ if [[ $(git tag -l --contains HEAD) ]]; then
     echo -n $(git tag -l --contains HEAD) > ../../cmd/ical-relay/VERSION
     echo -n $(git tag -l --contains HEAD) > ../../cmd/ical-notifier/VERSION
 else
-    echo -n $(git rev-parse --short HEAD) > ../../cmd/ical-relay/VERSION
-    echo -n $(git rev-parse --short HEAD) > ../../cmd/ical-notifier/VERSION
+    echo -n $(git describe --tags --abbrev=0) > ../../cmd/ical-relay/VERSION
+    echo -n $(git describe --tags --abbrev=0) > ../../cmd/ical-notifier/VERSION
+    echo -n "+" >> ../../cmd/ical-relay/VERSION
+    echo -n "+" >> ../../cmd/ical-notifier/VERSION
+    echo -n $(git rev-parse --short HEAD) >> ../../cmd/ical-relay/VERSION
+    echo -n $(git rev-parse --short HEAD) >> ../../cmd/ical-notifier/VERSION
 fi
 if ! git diff --quiet; then
     echo -n "-dirty" >> ../../cmd/ical-relay/VERSION
