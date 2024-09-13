ad228f725993dcc4a436050ab29ffde9ceb4bc0c
Julian Lemmerich <jle@cirosec.de>
2024-09-13 15:25:57 +0200

    wip(test/notifier): add changed testfile as file instead of base64

--- .github/sys-tests/testcase-4/test.sh
+++ .github/sys-tests/testcase-4/test.sh
@@ -7,7 +7,5 @@ sleep 5
 
 docker compose up -d ical-notifier
 
-docker compose exec mockmail cat /var/spool/mail/root
-
 # give the email from root but skip the first 9 lines
 diff -w email.eml <(docker compose exec mockmail tail -n +10 /var/spool/mail/root)
