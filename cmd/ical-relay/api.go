09a5b9257b760d4ae3fd729eec9fc948b9b77cf1
Wanthox <103198395+Wanthox@users.noreply.github.com>
2025-08-03 17:49:02 +0200

    (feat)relay: UI Feedback when using wrong times

--- cmd/ical-relay/api.go
+++ cmd/ical-relay/api.go
@@ -452,12 +452,12 @@ func newentryjsonApiHandler(w http.ResponseWriter, r *http.Request) {
 		if startErr != nil || endErr != nil {
 			requestLogger.Errorln(startErr)
 			requestLogger.Errorln(endErr)
-			http.Error(w, "Start- oder Endzeit ungültig!", http.StatusBadRequest)
+			http.Error(w, "Start- oder Endzeit ungültig!", http.StatusUnprocessableEntity)
 			return
 		}
 		if !end.After(start) {
 			requestLogger.Errorln("Endzeit muss nach Startzeit liegen!")
-			http.Error(w, "Endzeit muss nach Startzeit liegen!", http.StatusBadRequest)
+			http.Error(w, "Endzeit muss nach Startzeit liegen!", http.StatusUnprocessableEntity)
 			return
 		}
 
